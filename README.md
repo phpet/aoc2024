c7dc8ccf284f0f4b78b7ceeb82bb2c6be9e9cbf3
Philip Petrescu <philip.petrescu@caphyon.com>
2024-11-22 11:09:18 +0200

    added template

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aoc2024
 Advent of Code 2024
+
+# activate venv
+source ./venv/Scripts/activate
+
+# install dependencies
+pip install -r requirements.txt
---
+++ day01/template.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def solve(input_data):
+    # Implement your solution here
+    return None
+
+
+if __name__ == "__main__":
+    input_data = sys.stdin.read().strip()
+    print(solve(input_data))
